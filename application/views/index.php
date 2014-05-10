69115ffd5dcfbac096f21de96abd8d1da3fcb971
jdhrnndz <johndeniellehernandez@gmail.com>
2014-05-11 07:14:27 +0800

    Fetching Data from NOAH. Passing Data to Javascript.

--- application/views/index.php
+++ application/views/index.php
@@ -5,6 +5,12 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/bootstrap.min.css">
 </head>
 <body>
+	<hidden id="hidden_values" style="style:none;display:none;">
+	<?php
+		$content = file_get_contents('http://202.90.153.89/api/flood_maps');
+		print_r($content);
+	?>
+	</hidden>
 	<a href="#" onclick="AddWMSLayer();">Add WMS Layer</a> &nbsp;&nbsp;
     <a href="#" onclick="RemoveLayer();">Remove Layer</a> <br /><br />
     <div id="map_canvas" style="width: 100%; height: 600px;">
