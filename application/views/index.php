570c457204d7702924b124a41c56888f6174178d
Kim Joshua Advincula <kjcadvincula@gmail.com>
2014-05-11 02:10:13 +0800

    Other settings

--- application/views/index.php
+++ application/views/index.php
@@ -2,8 +2,13 @@
 <html>
 <head>
 	<title> .:: CheckInSaster ::. </title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/bootstrap.min.css">
 </head>
 <body>
-	
+
+	<div id="maps"></div>
+	<div id="overlays"></div>
 </body>
+
+<script type="text/javascript" src="<?php echo base_url();?>js/bootstrap.min.js"></script>
 </html>
