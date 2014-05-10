934fa6685c68145911c71f40db195de01929d3a5
jdhrnndz <johndeniellehernandez@gmail.com>
2014-05-11 02:11:26 +0800

    Maps Overlaying Prototype

--- application/views/index.php
+++ application/views/index.php
@@ -1,9 +1,14 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<title> .:: CheckInSaster ::. </title>
+	<script type="text/javascript" src="http://maps.google.com/maps/api/js?key=AIzaSyDs766uhPPeE9zxHfbsMOifMxffl7kbn_8&sensor=false"></script>
+	<script type="text/javascript" src="js/maps.js"></script>
 </head>
 <body>
-	
+	<a href="#" onclick="AddWMSLayer();">Add WMS Layer</a> &nbsp;&nbsp;
+    <a href="#" onclick="RemoveLayer();">Remove Layer</a> <br /><br />
+    <div id="map_canvas" style="width: 100%; height: 600px;">
+    </div>
 </body>
 </html>
