ef72302bf6f9c4ceb1c7da665a247e3f47896461
Kim Joshua Advincula <kjcadvincula@gmail.com>
2014-05-11 07:12:54 +0800

    somepushe

---
+++ application/views/includes/footer.php
@@ -0,0 +1,10 @@
+</body>
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.11.1.js"></script>
+<script type="text/javascript" src="<?php echo base_url();?>js/bootstrap.min.js"></script>
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?key=AIzaSyDs766uhPPeE9zxHfbsMOifMxffl7kbn_8&sensor=false"></script>
+<script type="text/javascript" src="js/maps.js"></script>
+
+</html>
+
+
